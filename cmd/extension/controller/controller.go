a0d9eba3e43b47078c88b061581c852c2cdd2519
Marin Atanasov Nikolov <dnaeon@gmail.com>
2026-02-09 17:50:13 +0200

    cmd/extension: move extension controller to its own package

--- cmd/extension/controller/controller.go
+++ cmd/extension/controller/controller.go
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-package main
+package controller
 
 import (
 	"context"
@@ -123,8 +123,8 @@ func getFlags(ctx context.Context) *flags {
 	return conf
 }
 
-// NewManagerCommand creates a new [cli.Command] for running the controller manager.
-func NewManagerCommand() *cli.Command {
+// New creates a new [cli.Command] for running the extension controller manager.
+func New() *cli.Command {
 	flags := flags{
 		gardenletFeatureGates: make(map[featuregate.Feature]bool),
 	}
@@ -274,7 +274,7 @@ func NewManagerCommand() *cli.Command {
 			// values to Helm, which our CLI app can pick up.
 			&cli.StringFlag{
 				Name:        "gardener-version",
-				Usage:       "version of gardener provided by gardenlet during deployment",
+				Usage:       "version of gardener provided by gardenlet or gardener-operator",
 				Destination: &flags.gardenerVersion,
 			},
 			&cli.StringMapFlag{
